d83ff2ace3f4d3224d2820c12d8e2a02fc7451c5
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-04-13 00:29:36 +0300

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,50 +1,58 @@
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = async(req, res) => {
-    const id = req.query.i;
-    if (id == undefined) { // no id
+module.exports = async (req, res) => {
+  const id = req.query.i;
+  if (id == undefined) {
+    // no id
+    res.json({
+      status: false,
+      msg: "Id is required",
+    });
+  } else if (id == "") {
+    // empty id
+    res.json({
+      status: false,
+      msg: "Id is required",
+    });
+  } else {
+    // id is ok
+    client.connect((err) => {
+      const db = client;
+      // connect with db
+      if (err) {
         res.json({
-            status: false,
-            msg: "Id is required"
-        })
-    } else if (id == "") { // empty id
-        res.json({
-            status: false,
-            msg: "Id is required"
-        })
-    } else { // id is ok 
-        MongoClient.connect(process.env.DB_URL, function(err, db) { // connect with db
-            if (err) {
-                res.json({
-                    status: false,
-                    msg: "Cannot connect with Database"
-                })
-            } else {
-                const search = { tid: id };
-                var dbo = db.db("shortner"); // db name
-                dbo.collection("data").find(search).toArray(function(errorr, result) {
-                    if (errorr) { // if error while fetching
-                        res.json({
-                            status: false,
-                            msg: "Error while fetch database"
-                        })
-                    } else {
-                        if (result[0] == undefined) {
-                            res.json({
-                                status: false,
-                                msg: "Cannot find a Url with the ID"
-                            })
-                        } else {
-                            res.json({ //if everything goes correct
-                                status: true,
-                                main_link: result[0].url,
-                                created_at: result[0].ist
-                            })
-                        };
-                    };
-                    db.close();
-                });
-            };
+          status: false,
+          msg: "Cannot connect with Database",
         });
-    };
+      } else {
+        const search = { tid: id };
+        var dbo = db.db("shortner"); // db name
+        dbo
+          .collection("data")
+          .find(search)
+          .toArray(function (errorr, result) {
+            if (errorr) {
+              // if error while fetching
+              res.json({
+                status: false,
+                msg: "Error while fetch database",
+              });
+            } else {
+              if (result[0] == undefined) {
+                res.json({
+                  status: false,
+                  msg: "Cannot find a Url with the ID",
+                });
+              } else {
+                res.writeHead(302, { Location: result[0].url });
+                res.end();
+              }
+            }
+            db.close();
+          });
+      }
+    });
+  }
 };
